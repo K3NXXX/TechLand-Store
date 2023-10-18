e20ee9c8e258d0670f84b5301816d9ede9dc60f2
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2023-10-18 21:45:40 +0300

    tried to fix images displaying again

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/",
+  base: "/techland-store/",
   resolve: {
-    alias: {
-      src: path.resolve('./'),
-    },
+    alias: [{ find: '@', replacement: './' }]
   }
   
 })
